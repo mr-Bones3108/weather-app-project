19c437cc8eefea1495006161820f16c4d62c8a77
mr-Bones3108 <nikhil788p@gmail.com>
2024-01-27 11:09:13 +0530

    finish inner working

--- src/index.js
+++ src/index.js
@@ -1,33 +1,3 @@
-const getTemperature = (()=>{
-
-    const convertData = (data)=>{
-      const name = data.location.name
-      const {
-        temp_c:temp_c,
-        temp_f:temp_f
-        } = data.current;
-      console.log(name)
-      console.log(temp_c)
-      console.log(temp_f)
-    }
-
-    async function getWeatherData(city){
-        const apiUrl = `https://api.weatherapi.com/v1/current.json?key=1fb9b24b0ecd46f3b25110719242301&q=${city}`;
-        
-        try{
-            const response = await fetch(apiUrl, {mode:'cors'})
-            if(!response.ok) throw new Error(`entered ${city} is not a city`)
-            const data = await response.json()
-            convertData(data)
-            console.log(data)
-            return data
-        }
-        catch(error){
-            alert(error)
-            return null
-        }
-}
-return{getWeatherData}
-})();
+import getTemperature from "./modules/app";
 
 getTemperature.getWeatherData("nagpur")
---
+++ src/modules/app.js
@@ -0,0 +1,36 @@
+const getTemperature = (()=>{
+    const convertData = (data)=>{
+      const name = data.location.name
+      const {
+        temp_c:temp_in_c,
+        feelslike_c:what_feelslike_c,
+        temp_f:temp_in_f,
+        feelslike_f:what_feelslike_f,
+        wind_kph:wind_in_kph,
+        humidity:what_humidity
+        } = data.current;
+       const condition = data.current.condition.text;
+        return{name,temp_in_c,what_feelslike_c,temp_in_f,what_feelslike_f,wind_in_kph,what_humidity,condition}
+    }
+
+    async function getWeatherData(city){
+        const apiUrl = `https://api.weatherapi.com/v1/current.json?key=1fb9b24b0ecd46f3b25110719242301&q=${city}`;
+        
+        try{
+            const response = await fetch(apiUrl, {mode:'cors'})
+            if(!response.ok) throw new Error(`entered ${city} is not a city`)
+            const data = convertData(await response.json())
+            // const data = await response.json()
+            console.log(data)
+            return data
+        }
+        catch(error){
+            alert(error)
+            return null
+        }
+}
+return{getWeatherData}
+})();
+
+export default getTemperature;
+
