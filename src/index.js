f5159854cd2ce349afe99c2428516817b1fc46c6
mr-Bones3108 <nikhil788p@gmail.com>
2024-01-27 15:26:50 +0530

    finish the project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import getTemperature from "./modules/app";
 import getCard from "./modules/view";
 
-const getData = await getTemperature.getWeatherData("nagpur")
-getCard.displayCard(getData)
+const form = document.getElementById("searchForm")
+const inputCity = document.getElementById("cityNameInput")
+const submitBtn = document.getElementById("searchBtn")
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+})
+
+submitBtn.addEventListener("click",async ()=>{
+    if(inputCity.value === "") return;
+    const getData = await getTemperature.getWeatherData(inputCity.value)
+    getCard.displayCard(getData)
+})
+
+
+
