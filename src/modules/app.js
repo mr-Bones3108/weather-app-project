a230306d51052b3d1d845682339262a7b4da91fe
mr-Bones3108 <nikhil788p@gmail.com>
2024-01-29 23:17:56 +0530

    add random weather codition img function

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,5 +1,5 @@
 const getTemperature = (()=>{
-    const convertData = (data)=>{
+    const convertData = async (data)=>{
       const name = data.location.name
       const {
         temp_c:temp_in_c,
@@ -10,7 +10,8 @@ const getTemperature = (()=>{
         humidity:what_humidity
         } = data.current;
        const condition = data.current.condition.text;
-        return{name,temp_in_c,what_feelslike_c,temp_in_f,what_feelslike_f,wind_in_kph,what_humidity,condition}
+       const gifImageUrl = await getWeatherConditionImage(condition)
+       return{name,temp_in_c,what_feelslike_c,temp_in_f,what_feelslike_f,wind_in_kph,what_humidity,condition,gifImageUrl}
     }
 
     async function getWeatherData(city){
@@ -26,7 +27,22 @@ const getTemperature = (()=>{
             alert(error)
             return null
         }
-}
+    }
+
+    async function getWeatherConditionImage(condition){
+        const conditionUrl = `https://api.giphy.com/v1/gifs/translate?api_key=fBYwmg7FlJvqtqxC3qON93YDNj3hpuUP&s=${condition}`;
+
+        try{
+            const response = await fetch(conditionUrl,{mode:'cors'})
+            if(!response.ok) throw new Error(`Condition Unclear`)
+            const data = await response.json()
+            return data.data.images.original.url;
+        }
+        catch(error){
+            alert(error)
+            return null
+        }
+    }
 return{getWeatherData}
 })();
 
--- src/modules/view.js
+++ src/modules/view.js
@@ -2,8 +2,13 @@ const getCard = (()=>{
     function displayCard(data){
         if(!data) return;
         const cardSection = document.getElementById("card-container")
+        const gifSection = document.getElementById("gifSection")
+        
         cardSection.classList.add("active")
+        gifSection.classList.add("active")
 
+
+        const gifImage = document.querySelector('img')
         const cityName = document.getElementById("cityName")
         const cityTemp = document.getElementById("cityTemp")
         const feelsLike = document.getElementById("feelsLike")
@@ -17,6 +22,7 @@ const getCard = (()=>{
         condition.innerText=`condition: ${data.condition}`
         humidity.innerText=`Humidity: ${data.what_humidity} %`
         wind.innerText=`Wind: ${data.wind_in_kph} km/h`
+        gifImage.src=data.gifImageUrl
     } 
     return {displayCard}
 })();
